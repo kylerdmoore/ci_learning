c2d3ff4f166c7135b2daa91ce63cfec8dc100a22
Kyler Moore <kylerdmoore@gmail.com>
2018-09-10 11:45:13 -0400

    Added bootstrap styles

---
+++ README.md
@@ -0,0 +1,3 @@
+# CI Learning
+
+[![Build Status](https://travis-ci.org/kylerdmoore/ci_learning.svg?branch=master)](https://travis-ci.org/kylerdmoore/ci_learning)
--- resources/views/calc.blade.php
+++ resources/views/calc.blade.php
@@ -1,15 +1,39 @@
-<form action="/calc" method="post">
-    {{ csrf_field() }}
-    <input placeholder="A" value="{{ $a }}" name="a">
-    <select name="action">
-        <option @if ($action == '+') selected="selected" @endif>+</option>
-        <option @if ($action == '-') selected="selected" @endif>-</option>
-        <option @if ($action == '*') selected="selected" @endif>*</option>
-        <option @if ($action == '/') selected="selected" @endif>/</option>
-    </select>
-    <input placeholder="B" value="{{ $b }}" name="b">
-    @if (isset($result))
-        <strong>= {{ $result }}</strong>
-    @endif
-    <button>Execute</button>
-</form>
+<html>
+
+<head>
+    <title>CI Learning</title>
+
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+</head>
+
+<body>
+    <div class="container text-center">
+        <h1 class="display-1">Kyler Learning CI/CD</h1>
+        <form class="form-inline justify-content-center" action="/calc" method="post">
+            {{ csrf_field() }}
+            <div class="form-group mr-3">
+                <input class="form-control" placeholder="A" value="{{ $a }}" name="a">
+            </div>
+            <div class="form-group mr-3">
+                <select class="form-control" name="action">
+                <option @if ($action == '+') selected="selected" @endif>+</option>
+                <option @if ($action == '-') selected="selected" @endif>-</option>
+                <option @if ($action == '*') selected="selected" @endif>*</option>
+                <option @if ($action == '/') selected="selected" @endif>/</option>
+            </select>
+            </div>
+            <div class="form-group mr-3">
+                <input class="form-control" placeholder="B" value="{{ $b }}" name="b">
+            </div>
+            <button class="btn btn-primary">Execute</button>
+        </form>
+
+        @if (isset($result))
+        <strong>Result</strong> = {{ $result }}
+        @endif
+    </div>
+
+    <script src="{{ mix('js/app.js') }}"></script>
+</body>
+
+</html>
